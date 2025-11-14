535f8fe99f6ae623a9c88fe547d334afc3423795
busayo-I <ibrahimbusayo2018@gmail.com>
2025-11-14 19:57:53 +0100

    make a correction

--- craftconnect/urls.py
+++ craftconnect/urls.py
@@ -15,19 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.conf import settings
-from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
         title="CraftConnect API",
         default_version='v1',
-        description="API documentation for CraftConnect Backend",
-        contact=openapi.Contact(email="teamcraftconnect@gmail.com"),
+        description="API documentation",
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
@@ -35,13 +32,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
     path('api/assessment/', include('assessments.urls')),
 
-    # Swagger documentation
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), 
+            name='schema-json'),
+
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-ui'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
